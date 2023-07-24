c889027e995e4292f3123af534b9f063743ba929
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-24 16:26:52 +0300

    fix

--- question1.c
+++ question1.c
@@ -1,50 +1,57 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...) {
-    char c;
-    va_list args;
-    int count;
-    count = 0;
-    va_start(args, format);
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c':
-                    c = (char)va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                case 's': {
-                    char *s = va_arg(args, char *);
-                    if (s) {
-                        while (*s) {
-                            putchar(*s);
-                            s++;
-                            count++;
-                        }
-                    }
-                    break;
-                }
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-
-    va_end(args);
-
-    return count;
+int _printf(const char *format, ...)
+{
+char c;
+va_list args;
+int count;
+count = 0;
+va_start(args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+c = (char)va_arg(args, int);
+putchar(c);
+count++;
+break;
+case 's':
+{
+char *s = va_arg(args, char *);
+if (s)
+{
+while (*s)
+{
+putchar(*s);
+s++;
+count++;
+}
+}
+break;
+}
+case '%':
+putchar('%');
+count++;
+break;
+default:
+putchar('%');
+putchar(*format);
+count += 2;
+break;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
 }
