58624c6b1288257e88d87f710a1527dd4c025afe
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-25 12:26:41 +0300

    fix

--- question1.c
+++ question1.c
@@ -1,8 +1,5 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-
 /**
  *_printf - this function takes a format string and a variable number of arguments,
  * and processes the format specifiers within the format string to print the
@@ -12,40 +9,57 @@
  *            in the format string.
  * Return: The total number of characters printed (excluding the null byte used to end output to strings).
  */
-int _printf(const char *format, ...) {
-    int printed_chars = 0;
-    va_list args;
-    va_start(args, format);
-    while (*format) {
-        if (*format == '%') {
-            format++; // Move past the '%'
-
-            if (*format == 'c') {
-                // char type
-                int ch = va_arg(args, int);
-                putchar(ch);
-                printed_chars++;
-            } else if (*format == 's') {
-                // string type
-                const char *str = va_arg(args, const char*);
-                printed_chars += fputs(str, stdout);
-            } else if (*format == '%') {
-                // '%' character
-                putchar('%');
-                printed_chars++;
-            } else {
-                // Unknown format specifier, just print the character
-                putchar(*format);
-                printed_chars++;
-            }
-        } else {
-            // Regular character, not a format specifier
-            putchar(*format);
-            printed_chars++;
-        }
-        format++;
-    }
-    va_end(args);
- return printed_chars;
+int _printf(const char *format, ...)
+{
+char c;
+va_list args;
+int count;
+count = 0;
+va_start(args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+c = (char)va_arg(args, int);
+putchar(c);
+count++;
+break;
+case 's':
+{
+char *s = va_arg(args, char *);
+if (s)
+{
+while (*s)
+{
+putchar(*s);
+s++;
+count++;
 }
-
+}
+break;
+}
+case '%':
+putchar('%');
+count++;
+break;
+default:
+putchar('%');
+putchar(*format);
+count += 2;
+break;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
+}
