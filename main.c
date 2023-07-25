cb27fb84a111696ca0794af77c9eb5053f25141d
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-25 09:36:34 +0300

    fix

--- main.c
+++ main.c
@@ -38,6 +38,10 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
+    _printf("%d\n", len);
+    printf("%d\n", len2);
+    _printf("%i\n", len);
+    printf("%i\n", len2);
     return (0);
 }
 
