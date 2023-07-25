184c9dcc3910c3907f5df2a7d7dd5a7c76c00bc2
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-25 12:21:14 +0300

    mll

--- question1.c
+++ question1.c
@@ -12,73 +12,40 @@
  *            in the format string.
  * Return: The total number of characters printed (excluding the null byte used to end output to strings).
  */
-#include <stdio.h>
-#include <stdarg.h>
-
-
 int _printf(const char *format, ...) {
     int printed_chars = 0;
     va_list args;
     va_start(args, format);
-
     while (*format) {
         if (*format == '%') {
             format++; // Move past the '%'
 
-            if (*format == 'c')
-                putchar(*format);
-            else if (*format == 's')
-                fputs(format, stdout);
-            else if (*format == '%')
+            if (*format == 'c') {
+                // char type
+                int ch = va_arg(args, int);
+                putchar(ch);
+                printed_chars++;
+            } else if (*format == 's') {
+                // string type
+                const char *str = va_arg(args, const char*);
+                printed_chars += fputs(str, stdout);
+            } else if (*format == '%') {
+                // '%' character
                 putchar('%');
-            else if (*format == 'd')
-                printf("%d", va_arg(args, int));
-            else if (*format == 'i')
-                printf("%i", va_arg(args, int));
-            else if (* format == 'b'){
-                int num = va_arg(args, int);
-                char* binary_str = int_to_binary(num);
-                printf(binary_str);
+                printed_chars++;
+            } else {
+                // Unknown format specifier, just print the character
+                putchar(*format);
+                printed_chars++;
             }
         } else {
             // Regular character, not a format specifier
             putchar(*format);
+            printed_chars++;
         }
-        printed_chars++;
         format++;
     }
-
     va_end(args);
-
-    return printed_chars;
+ return printed_chars;
 }
 
-char* int_to_binary(int num) {
-    int i;
-    int index = 0;
-    int leading_zeros = 1;
-    int size = sizeof(int) * 8; // Number of bits in an integer
-    char* binary_str = (char*)malloc(size + 1); // +1 for null terminator
-
-    if (!binary_str) {
-        printf("Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Extract each bit from the integer and build the binary string
-    for (i = size - 1; i >= 0; i--) {
-        int bit = (num & (1 << i)) ? 1 : 0;
-
-        if (bit || !leading_zeros) {
-            binary_str[index++] = bit + '0';
-            leading_zeros = 0;
-        }
-    }
-
-    // If the number was zero, represent it as '0'
-    if (index == 0)
-        binary_str[index++] = '0';
-
-    binary_str[index] = '\0'; // Null-terminate the binary string
-    return binary_str;
-}
